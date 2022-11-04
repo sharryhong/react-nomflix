41ccbc4baf8e79b3f7ca26323e6577f555df12eb
sharryhong <kshopzoa15@gmail.com>
2022-11-04 15:11:13 +0900

    [init]

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./styles/theme";
+import GlobalStyle from "./styles/globalStyle";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
