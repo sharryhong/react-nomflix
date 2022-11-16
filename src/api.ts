0abd11f0684fac81a2e6af8d0e956faab2793c77
sharryhong <kshopzoa15@gmail.com>
2022-11-16 16:45:07 +0900

    환경변수 수정

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
-const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getMovies = () => {
   return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then(
